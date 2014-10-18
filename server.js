c3229c157602a155374d01c3cff66f4c003d9c25
hschafer <hschafer@uw.edu>
2014-10-17 22:25:29 -0700

    Organizing
    Organizing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 "use strict";
 var http = require("http");
+var port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello fellow hackers");
   response.end();
-}).listen(80);
+}).listen(port);
