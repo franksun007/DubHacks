01187a72bb67ca3f9300ea0fad3dcd2c4c3c711c
hschafer <hschafer@uw.edu>
2014-10-18 01:55:26 -0700

    fixing some semicolons

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.use(express.static(__dirname + '/client/'));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/client/html/index.html');
-})
+});
 
 app.listen(process.env.PORT || 8080);
