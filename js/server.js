083b7f5768a5cdd8a52ee315292f86d66eea9e9b
PTC <pheartheceal@gmail.com>
2014-10-17 18:41:12 -0700

    remove old

--- js/server.js
+++
@@ -1,8 +0,0 @@
-
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello fellow hackers");
-  response.end();
-}).listen(80);
