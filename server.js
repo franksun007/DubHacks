349012a0caf2e6c45da6b206ae0b6aca6154705b
hschafer <hschafer@uw.edu>
2014-10-17 21:59:03 -0700

    moved server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+"use strict";
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello fellow hackers");
+  response.end();
+}).listen(80);
