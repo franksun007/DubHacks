3ba5a9d34482752fd9857adc1b88338ca3ff11be
jatcar95 <jacobtcarlson@gmail.com>
2014-10-18 11:31:11 -0700

    just more personal files
    that i have to commit :P

--- client/users/jake/AI.js
+++ client/users/jake/AI.js
@@ -1,1 +1,14 @@
-{ "get_next_moves" : function(germs) { return {"direction":"left", "command":"move"}; } }
+{
+	"get_next_moves": function(germs) {
+		var command = [];
+		for (var i = 0; i < germs.length; i++) {
+			// surroundings = {"left" : "", "right": "", "up" : "", "down" : ""};
+			var direction = ["left", "right", "up", "down"];
+			var action = ["move", "split"];
+			var actionindex = parseInt(Math.random() * 2);
+			var directionindex = parseInt(Math.random() * 4);
+			command[i] = {"command":action[actionindex], "direction": direction[directionindex]};
+		}
+		return command;
+	}
+}
