bcd137cb6af0dfabd10e26f69ce7cbcaba7a2423
jatcar95 <jacobtcarlson@gmail.com>
2014-10-18 02:23:21 -0700

    added username/password form

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/client/'));
+app.use(bodyParser());
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/client/html/index.html');
-})
+});
+
+app.get('/form', function (req, res) {
+	res.sendFile(__dirname + '/client/html/form.html');
+});
+
+app.post('/verify', function (req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	res.send(req.body);
+});
 
 app.listen(process.env.PORT || 8080);
